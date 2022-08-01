e0e7c901b366919012c2622c8a00a623de8aa3af
Hunts16 <101857008+Hunts16@users.noreply.github.com>
2022-07-31 19:15:11 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "b5mrcbbpNaUrEvwzveZDzJml7dHSdwtMZsasCwAk3O3WdkNqBthLErbDdXkUekM1";
 /**
  * Get list of teams in event
  *
